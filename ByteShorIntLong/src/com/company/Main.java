1f749ef535c16af7b5ed426b79fa370b89ecd59c
ks603 <kevin.shaughnessy18@gmail.com>
2020-10-26 14:44:22 -0600

    Showing overflow and underflow in Java

--- ByteShorIntLong/src/com/company/Main.java
+++ ByteShorIntLong/src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum value = " + myMinIntValue);
+        System.out.println("Integer Minimum value = " + myMaxIntValue);
+        System.out.println("Busted MAx value = " + (myMaxIntValue + 1));
+        System.out.println("Busted Min value " + (myMinIntValue - 1));
+
     }
 }
