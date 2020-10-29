14e1b10b8bf037814bbcb46b264de8af3f5b484f
ks603 <kevin.shaughnessy18@gmail.com>
2020-10-29 14:02:37 -0600

    Java practice with Booleans and Ternary Operators

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -59,5 +59,22 @@ public class Main {
         if (topScore > 90 || secondTopScore <= 90) {
             System.out.println("Either or both of the conditions are true");
         }
+
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if (isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+        isCar = true;
+
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("was car was true");
+        }
+        System.out.println(wasCar);
     }
 }
