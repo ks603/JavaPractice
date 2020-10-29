64acb9fbf542e900efc1c6170785440888aabcc1
ks603 <kevin.shaughnessy18@gmail.com>
2020-10-28 23:49:06 -0600

    Practice with Java Operators

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -3,47 +3,61 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	 int result = 1 + 2;
+        int result = 1 + 2;
         System.out.println("1 + 2 = " + result);
-	 int previousResult = result;
+        int previousResult = result;
         System.out.println("previousResult = " + previousResult);
-     result = result - 1;
+        result = result - 1;
         System.out.println("3 - 1 = " + result);
         System.out.println("previousResult = " + previousResult);
 
-     result = result * 10;
+        result = result * 10;
         System.out.println("2 * 10 = " + result);
 
-     result = result / 5;
+        result = result / 5;
         System.out.println("20 / 5 = " + result);
 
-     result = result % 3;
+        result = result % 3;
         System.out.println("4 % 3 = " + result);
 
-     result++;
+        result++;
         System.out.println("1 + 1 = " + result);
 
-     result--;
+        result--;
         System.out.println("2 - 1 = " + result);
 
-     result += 2;
+        result += 2;
         System.out.println("1 + 2 = " + result);
 
 
-     result *= 10;
+        result *= 10;
         System.out.println("3 * 30 = " + result);
 
-     result /= 3;
+        result /= 3;
         System.out.println("30 / 3 = " + result);
 
-     result -= 2;
+        result -= 2;
 
         System.out.println("10 - 2 = " + result);
 
-     boolean isAlien = false;
-     if (isAlien == false) {
-         System.out.println("it is not an alien!");
-         System.out.println("And I am scared of aliens");
-     }
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("it is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+
+        int topScore = 82;
+        if (topScore < 100) {
+            System.out.println("You got the high score");
+        }
+
+        int secondTopScore = 81;
+        if(topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if (topScore > 90 || secondTopScore <= 90) {
+            System.out.println("Either or both of the conditions are true");
+        }
     }
 }
