f6c9070dac21283ec03076bce31d199481d76ee0
ks603 <kevin.shaughnessy18@gmail.com>
2020-11-09 16:25:23 -0500

    Practice with expressions ans statements

--- Keywords and Expressions/src/com/company/Main.java
+++ Keywords and Expressions/src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 //	a mile is equal to 1.609344 Kilometres
         double kilometres = (100 * 1.609344);
         int highScore = 50;
-        if(highScore == 50) {
+        if (highScore == 50) {
             System.out.println("This is an expression");
         }
 
@@ -17,8 +17,37 @@ public class Main {
         System.out.println("This is" +
                 " another" +
                 " Still more.");
-        int anotherVariable = 5;anotherVariable--;
+        int anotherVariable = 5;
+        anotherVariable--;
         System.out.println(anotherVariable);
+
+
+        boolean gameOver = true;
+        int levelCompleted = 5;
+        int bonus = 100;
+        int score = 800;
+
+//        if(score <5000 && score >1000) {
+//            System.out.println("Score was less than or equal to");
+//        }  else if (score < 1000) {
+//            System.out.println("Your score wass less than 1000");
+//        } else {
+//            System.out.println("Got here");
+//        }
+       if( gameOver) {
+           int finalScore = score + (levelCompleted * bonus);
+           System.out.println(" Your final score was " + finalScore);
+       }
+
+        boolean newGameOver = true;
+        int levelCompleted2 = 8;
+        int newBonus = 200;
+        int newScore = 10000;
+        if (newGameOver) {
+            int newFinalScore = newScore + (levelCompleted2 * newBonus);
+            System.out.println("Your new score was " + newFinalScore);
+
+        }
 
     }
 }
