1de5b2ed5d6bcae1b1a60bcc175bcd4b3fe21389
ks603 <kevin.shaughnessy18@gmail.com>
2020-11-09 12:40:57 -0500

    Practice with expressions and statements

---
+++ Keywords and Expressions/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	a mile is equal to 1.609344 Kilometres
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+        if(highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+        System.out.println("This is" +
+                " another" +
+                " Still more.");
+        int anotherVariable = 5;anotherVariable--;
+        System.out.println(anotherVariable);
+
+    }
+}
