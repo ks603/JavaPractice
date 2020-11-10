5d565acef77423858d1cdceb7a0d515ce65058e3
ks603 <kevin.shaughnessy18@gmail.com>
2020-11-10 15:39:13 -0500

    Speed converter test

---
+++ SampleCodingExcercise/src/PositiveNegativeZero.java
@@ -0,0 +1,14 @@
+
+
+public class PositiveNegativeZero {
+
+    public static void main(String[] args) {
+        checkNumber(-5);
+        System.out.println("yo");
+    }
+    public static void checkNumber(int number) {
+        if(number > 0) {
+            System.out.println("positive");
+        }
+    }
+}
