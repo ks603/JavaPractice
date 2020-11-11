94140f596cb6f8d14c1206cf02f0b85a77337a44
ks603 <kevin.shaughnessy18@gmail.com>
2020-11-11 17:17:08 -0500

    Fixed function to return the value I wanted as asked

--- MethodOverload/src/com/company/Main.java
+++ MethodOverload/src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
         calculateScore(75);
         calculateScore();
         System.out.println(calcFeetAndInchesToCentimeters(4, 8));
-        calcFeetAndInchesToCentimeters(50);
+        System.out.println(calcFeetAndInchesToCentimeters(50));
     }
     public static int calculateScore(String playerName, int score) {
         System.out.println("PLayer " + playerName + " scored " + score + " points");
@@ -45,16 +45,15 @@ public class Main {
     public static double calcFeetAndInchesToCentimeters(double inches) {
         double someVar = 0;
         double someNewVar = 0;
+        double inchesToFeet = 0;
         if (inches <= 0) {
             return -1;
         } else {
             someVar = inches % 12;
             someNewVar = Math.floor(inches / 12);
-            System.out.println(someVar);
-            System.out.println(someNewVar);
-            System.out.println(calcFeetAndInchesToCentimeters(someNewVar, someVar));
+            inchesToFeet = calcFeetAndInchesToCentimeters(someNewVar, someVar);
 
-        } return someVar;
+        } return inchesToFeet;
     }
 
 }
