6d7ace4a439b44f666eeae17ab6daa0cf936bc47
ks603 <kevin.shaughnessy18@gmail.com>
2020-11-11 20:46:26 -0500

    Updated answer for Minutes and Seconds challenge

--- SecondsAndMinutesChallenge/src/com/company/Main.java
+++ SecondsAndMinutesChallenge/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(65, 8));
         System.out.println(getDurationString(63145));
     }
 
@@ -12,20 +12,34 @@ public class Main {
         if (minutes < 0 || seconds < 0 || seconds > 59) {
             System.out.println("invalid Input");
             return "invalid Input";
-        } else {
+        }
             int hours = minutes / 60;
             minutes = minutes % 60;
-            return hours + "h " + minutes + "m " + seconds + "s";
+
+            String hoursString = hours + "h ";
+            if ( hours < 10) {
+                hoursString = "0" + hours + "h ";
+            }
+
+            String minutesString = minutes + "m ";
+            if (minutes < 10) {
+                minutesString = "0" + minutes + "m ";
+            }
+
+        String secondsString = seconds + "s ";
+        if ( seconds < 10) {
+            secondsString = "0" + seconds + "s ";
         }
+
+        return hoursString + minutesString + secondsString;
     }
     public static String getDurationString(int seconds) {
         if (seconds <= 0) {
             System.out.println("invalid Input");
             return "invalid Input";
-        } else {
+        }
             int minutes = seconds / 60;
             seconds = seconds % 60;
             return getDurationString( minutes, seconds);
-        }
     }
 }
